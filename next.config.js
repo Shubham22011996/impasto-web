e8c0c04adb5812a8c7618e64c15cc35dc3c57e5f
shubham rana <srana@rpmmoves.com>
2026-05-11 14:21:44 +0530

    fix vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-
   images: {
     unoptimized: true,
   },
-
-  basePath: "/impasto-web",
-  assetPrefix: "/impasto-web/",
 };
 
 module.exports = nextConfig;
